8713cda5f352018e220a0d944efdb929e58d3962
whyext <159469635+whyext@users.noreply.github.com>
2024-03-05 19:53:17 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,20 +2,20 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for MusicMaker</h1>
-            <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
+            <h1 className="font-semibold text-3xl text-white/75">Sukuna'ın Gizlilik Politikası</h1>
+            <p className="text-sm text-white/30 mb-4">Son Güncelleme: 05/03/2024  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the MusicMaker bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ Sukuna botunu kullanmak için discord sunucunuza eklerken gerekli izinleri tanımlamanız gerekir.. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ Bot komutlarından belirli komutları kullanırken <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">Sukuna</a>  botuna oy vermeniz gerekmektedir .</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the MusicMaker bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Her kullanıcı, zarar verme niyetinde olmadığı sürece Sukuna botunu dilediği gibi kullanabilir.</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using MusicMaker.</p>   
+        <p>⊳ Sukuna'yı kullanırken belirli komutları aynı anda kullanamazsınız.</p>   
         <div className="py-3"></div>
-        <p>⊳ MusicMaker bot does not save any data based on users.</p>
+        <p>⊳ Sukuna bot, kullanıcılara dayalı hiçbir veriyi kaydetmez.</p>
         <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+        <p>⊳ Botu kullanırken kullanıcı kimliğiniz bir defaya mahsus olmak üzere 60 saniye süreyle sisteme kaydedilip silinir.</p> 
     
         </div>
             </div>
