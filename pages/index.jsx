3e5d7356cc58a17b9d635c43dddf6a6e5680130b
whyext <159469635+whyext@users.noreply.github.com>
2024-03-05 22:46:41 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -86,11 +86,11 @@ Vote</a>
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://cdn.discordapp.com/attachments/1208571557836750901/1214659137515036692/maxresdefault.jpg?ex=65f9ea79&is=65e77579&hm=dd819f4057b7e7455c283cba76f4f6e682151a2538b94689380660a5bdd53af5&"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <img src="https://cdn.discordapp.com/attachments/1208571557836750901/1214660143866847333/icegif-559.gif?ex=65f9eb69&is=65e77669&hm=a4313e68e28cc5c7fdb3021cc368f5d2b1fcef41f93dacec9372b25388987d22&"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
                   <p className="text-3xl text-white font-semibold">Botunuzu En İyi Şekilde Kullanın</p>
                   <p className="text-md text-white font-medium text-gray-500 line-clamp-5">En iyi ve en güncel robot olan SukunaBot ile en iyi partileri düzenlemeye hazır mısınız?</p>
                 </div>
-                <img src="https://cdn.discordapp.com/attachments/1208571557836750901/1214659137515036692/maxresdefault.jpg?ex=65f9ea79&is=65e77579&hm=dd819f4057b7e7455c283cba76f4f6e682151a2538b94689380660a5bdd53af5&" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://cdn.discordapp.com/attachments/1208571557836750901/1214660143866847333/icegif-559.gif?ex=65f9eb69&is=65e77669&hm=a4313e68e28cc5c7fdb3021cc368f5d2b1fcef41f93dacec9372b25388987d22&" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
