57790c251cdb8fd92af483188bca7a33b083c7d8
whyext <159469635+whyext@users.noreply.github.com>
2024-03-07 19:26:56 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,7 +37,7 @@ export default function AwardApp({ Component, pageProps }) {
       name: "Destek",
       icon: "fab fa-discord",
       activeIcon: "fab fa-discord",
-      href: "https://discord.gg/ZcDprCrFTn",
+      href: "https://discord.gg/whydow",
     },
     {
       link: true,
