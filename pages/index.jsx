98a7540c8333a8ce6f03823f26889ba7d75479e2
whyext <159469635+whyext@users.noreply.github.com>
2024-03-09 16:10:26 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -27,7 +27,7 @@ export default function Index() {
               </a>
             </Link>
   <div className="py-10"></div>
-  <Link href={"https://top.gg/bot/774043716797071371/vote"}>
+  <Link href={"#"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Vote</a>
   </Link>
