224ab0dc93c6b10705683f4a257ddb69852110f4
whyext <159469635+whyext@users.noreply.github.com>
2024-03-09 16:11:41 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -103,7 +103,7 @@ Destek Sunucu</a>
 
   <div className="py-10"></div>
   
-  <Link href={"https://top.gg/bot/774043716797071371/vote"}>
+  <Link href={"#"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Oy Sukuna</a>
   </Link>
