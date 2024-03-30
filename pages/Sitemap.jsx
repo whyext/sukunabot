f7eec7f759f271b0cb7ea052ced17a26f0d812e6
whyext <159469635+whyext@users.noreply.github.com>
2024-03-30 14:10:25 +0300

    Update Sitemap.jsx

--- pages/Sitemap.jsx
+++ pages/Sitemap.jsx
@@ -3,35 +3,36 @@ import React from 'react';
 
 const Sitemap = () => {
   const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        <url>
-            <loc>https://sukunabot.vercel.app/</loc>
-            <lastmod>2024-03-30T10:45:50+00:00</lastmod>
-            <priority>1.00</priority>
-        </url>
-        <url>
-            <loc>https://sukunabot.vercel.app/commands</loc>
-            <lastmod>2024-03-30T10:45:50+00:00</lastmod>
-            <priority>0.80</priority>
-        </url>
-        <url>
-            <loc>https://sukunabot.vercel.app/partners</loc>
-            <lastmod>2024-03-30T10:45:50+00:00</lastmod>
-            <priority>0.80</priority>
-        </url>
-        <url>
-            <loc>https://sukunabot.vercel.app/tos</loc>
-            <lastmod>2024-03-30T10:45:50+00:00</lastmod>
-            <priority>0.80</priority>
-        </url>
-        <url>
-            <loc>https://sukunabot.vercel.app/privacy</loc>
-            <lastmod>2024-03-30T10:45:50+00:00</lastmod>
-            <priority>0.80</priority>
-        </url>
-    </urlset>`;
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://sukunabot.vercel.app/</loc>
+        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
+        <priority>1.00</priority>
+    </url>
+    <url>
+        <loc>https://sukunabot.vercel.app/commands</loc>
+        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://sukunabot.vercel.app/partners</loc>
+        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://sukunabot.vercel.app/tos</loc>
+        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>https://sukunabot.vercel.app/privacy</loc>
+        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+</urlset>`;
 
-  return <>{xmlContent}</>;
+  return <pre>{xmlContent}</pre>;
 };
 
 export default Sitemap;
+
