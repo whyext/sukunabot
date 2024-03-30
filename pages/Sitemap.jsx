65a70a78f2bec1e7a83414a9414013b2486b7818
whyext <159469635+whyext@users.noreply.github.com>
2024-03-30 14:13:50 +0300

    Update Sitemap.jsx

--- pages/Sitemap.jsx
+++ pages/Sitemap.jsx
@@ -1,38 +1,15 @@
 import Link from "next/link";
 import React from 'react';
 
-const Sitemap = () => {
-  const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
-        <loc>https://sukunabot.vercel.app/</loc>
-        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
-        <priority>1.00</priority>
-    </url>
-    <url>
-        <loc>https://sukunabot.vercel.app/commands</loc>
-        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
-        <priority>0.80</priority>
-    </url>
-    <url>
-        <loc>https://sukunabot.vercel.app/partners</loc>
-        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
-        <priority>0.80</priority>
-    </url>
-    <url>
-        <loc>https://sukunabot.vercel.app/tos</loc>
-        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
-        <priority>0.80</priority>
-    </url>
-    <url>
-        <loc>https://sukunabot.vercel.app/privacy</loc>
-        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
-        <priority>0.80</priority>
-    </url>
-</urlset>`;
+import React from 'react';
 
-  return <pre>{xmlContent}</pre>;
+const Sitemap = () => {
+  return (
+    <div>
+      <h1>Sitemap</h1>
+      <iframe src="/sitemap.xml" title="Sitemap" width="100%" height="600px" />
+    </div>
+  );
 };
 
 export default Sitemap;
-
