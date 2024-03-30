3a0ffd384e83dd2efc6a28720adfd0402339dbd7
whyext <159469635+whyext@users.noreply.github.com>
2024-03-30 14:07:20 +0300

    Update Sitemap.jsx

--- pages/Sitemap.jsx
+++ pages/Sitemap.jsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import React from 'react';
 
-const Sitemap = () => {
-  const xmlContent = `
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
@@ -36,5 +34,4 @@ const Sitemap = () => {
   return <>{xmlContent}</>;
 };
 
-export default Sitemap;
 
