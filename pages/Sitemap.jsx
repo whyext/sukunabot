041ef2f4897af598cc7e31b36f6893a243a331f8
whyext <159469635+whyext@users.noreply.github.com>
2024-03-30 14:05:06 +0300

    Update Sitemap.jsx

--- pages/Sitemap.jsx
+++ pages/Sitemap.jsx
@@ -3,39 +3,34 @@ import React from 'react';
 
 const Sitemap = () => {
   const xmlContent = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-      <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
-      <url>
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
         <loc>https://sukunabot.vercel.app/</loc>
         <lastmod>2024-03-30T10:45:50+00:00</lastmod>
         <priority>1.00</priority>
-      </url>
-      <url>
+    </url>
+    <url>
         <loc>https://sukunabot.vercel.app/commands</loc>
         <lastmod>2024-03-30T10:45:50+00:00</lastmod>
         <priority>0.80</priority>
-      </url>
-      <url>
+    </url>
+    <url>
         <loc>https://sukunabot.vercel.app/partners</loc>
         <lastmod>2024-03-30T10:45:50+00:00</lastmod>
         <priority>0.80</priority>
-      </url>
-      <url>
+    </url>
+    <url>
         <loc>https://sukunabot.vercel.app/tos</loc>
         <lastmod>2024-03-30T10:45:50+00:00</lastmod>
         <priority>0.80</priority>
-      </url>
-      <url>
+    </url>
+    <url>
         <loc>https://sukunabot.vercel.app/privacy</loc>
         <lastmod>2024-03-30T10:45:50+00:00</lastmod>
         <priority>0.80</priority>
-      </url>
-    </urlset>
+    </url>
+</urlset>
   `;
 
   return <>{xmlContent}</>;
