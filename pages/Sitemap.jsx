c4e30b34b004ae92c87841118f328263e097f7d3
whyext <159469635+whyext@users.noreply.github.com>
2024-03-30 13:47:33 +0300

    Create Sitemap.jsx

---
+++ pages/Sitemap.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Sitemap = () => {
+  return (
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      {/* created with Free Online Sitemap Generator www.xml-sitemaps.com */}
+      <url>
+        <loc>https://sukunabot.vercel.app/</loc>
+        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
+        <priority>1.00</priority>
+      </url>
+      <url>
+        <loc>https://sukunabot.vercel.app/commands</loc>
+        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
+        <priority>0.80</priority>
+      </url>
+      <url>
+        <loc>https://sukunabot.vercel.app/partners</loc>
+        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
+        <priority>0.80</priority>
+      </url>
+      <url>
+        <loc>https://sukunabot.vercel.app/tos</loc>
+        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
+        <priority>0.80</priority>
+      </url>
+      <url>
+        <loc>https://sukunabot.vercel.app/privacy</loc>
+        <lastmod>2024-03-30T10:45:50+00:00</lastmod>
+        <priority>0.80</priority>
+      </url>
+    </urlset>
+  );
+}
+
+export default Sitemap;
