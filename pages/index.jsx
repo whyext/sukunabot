7cbe9f205112eb942e52d79ac2ac7ce6b4f0eb86
whyext <159469635+whyext@users.noreply.github.com>
2024-04-04 21:09:47 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -32,13 +32,6 @@ export default function Index() {
 Vote</a>
   </a>
   </Link>	  
-<div className="py-10">
-  <Link href="#">
-    <a className=" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-300 text-white ">
-      Kontrol Paneli
-    </a>
-  </Link>
-</div>
         <div style={{ zIndex: '-1' }} className="hidden xl:block undrag pointer-events-none opacity-10 -left-[10vw] absolute top-[8vw] h-[35vw] flex-shrink-0 grayscale rotate-[1deg]">
         <Image width="600" height="600" src="/img/gift-left.png"  />
         </div>
